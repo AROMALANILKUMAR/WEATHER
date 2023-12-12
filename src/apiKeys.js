455401937594930d597d2e9e31cfc784b9242a5a
AROMALANILKUMAR <aromalanilkumar.621821@gmail.com>
2023-12-12 09:09:15 +0530

    done

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "aa337922d83aeed1d0d6dd26d187629b",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
